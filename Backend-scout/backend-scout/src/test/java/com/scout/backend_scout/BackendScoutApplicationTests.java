00563ddb4c8b960dfd72b0082159367ddc8e0d19
jveneg12 <164574149+jveneg12@users.noreply.github.com>
2026-09-02 00:24:52 -0400

    Back-end
    Microservicio con conexion a la base de datos

---
+++ Backend-scout/backend-scout/src/test/java/com/scout/backend_scout/BackendScoutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scout.backend_scout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendScoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
