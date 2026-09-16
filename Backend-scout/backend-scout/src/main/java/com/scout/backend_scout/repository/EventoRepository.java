204410979d78c3a06dbcbd71083f93f2f5530645
jveneg12 <jasoncristian.venegas@gmail.com>
2026-09-16 20:59:38 -0300

    usuarios funcionando, eventos deletados, testes funcionando

--- Backend-scout/backend-scout/src/main/java/com/scout/backend_scout/repository/EventoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.scout.backend_scout.repository;
-
-import com.scout.backend_scout.model.Evento;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EventoRepository extends JpaRepository<Evento, Long> {
-}
