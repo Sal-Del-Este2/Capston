f628b39466be63d0d8d5e8671f330e942508557d
jveneg12 <jasoncristian.venegas@gmail.com>
2026-09-22 23:08:39 -0300

    finanzas con pasarela de pago funcional con MercadoPago, se agregaron las clases Pago, PagoController, PagoRepository y MercadoPagoService. Se agregaron los archivos de configuración de Maven y se actualizaron los archivos de propiedades de la aplicación para incluir la configuración de MercadoPago. Además, se realizaron cambios en el frontend para integrar la funcionalidad de pago.

---
+++ Backend-scout/backend-scout-finanzas/src/main/java/com/scout/backend_scout_finanzas/BackendScoutFinanzasApplication.java
@@ -0,0 +1,13 @@
+package com.scout.backend_scout_finanzas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendScoutFinanzasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendScoutFinanzasApplication.class, args);
+	}
+
+}
